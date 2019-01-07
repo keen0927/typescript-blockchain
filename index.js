746eadffaf8e698398f44d99a0cfac9a5714a573
KEEN <kyh0927@live.co.kr>
2019-01-07 22:30:00 +0900

    typescript test

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'keen', age = 36, gender = 'mail';
+const sayHi = (name, age, gender) => {
+    console.log(`${name}, is ${age}years old. and ${gender}`);
+};
+sayHi(name, age);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = 'keen',
+      age = 36,
+      gender = 'mail';
+
+const sayHi = (name, age, gender) => {
+  console.log(`${name}, is ${age}years old. and ${gender}`)
+}
+
+sayHi(name, age, gender);
+
+export{}
